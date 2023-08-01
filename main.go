bd65ad68eb3fa9b40c8e83a9bdf94324379da1b1
k0yote <yjmk0yote@gmail.com>
2023-08-02 04:51:52 +0900

    transport layer

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"time"
+
+	"github.com/k0yote/privatechain/network"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	trLocal := network.NewLocalTransport("LOCAL")
+	trRemote := network.NewLocalTransport("REMOTE")
+
+	trLocal.Connect(trRemote)
+	trRemote.Connect(trLocal)
+
+	go func() {
+		for {
+			trRemote.SendMessage(trLocal.Addr(), []byte("hello world"))
+			time.Sleep(1 * time.Second)
+		}
+	}()
+
+	opts := network.ServerOpts{
+		Transports: []network.Transport{trLocal},
+	}
+
+	s := network.NewServer(opts)
+	s.Start()
 }
---
+++ network/local_transport_test.go
@@ -0,0 +1,32 @@
+package network
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestConnect(t *testing.T) {
+	tra := NewLocalTransport("A")
+	trb := NewLocalTransport("B")
+
+	tra.Connect(trb)
+	trb.Connect(tra)
+	assert.Equal(t, tra.Addr(), NetAddr("A"))
+	assert.Equal(t, trb.Addr(), NetAddr("B"))
+}
+
+func TestSendMessge(t *testing.T) {
+	tra := NewLocalTransport("A")
+	trb := NewLocalTransport("B")
+
+	tra.Connect(trb)
+	trb.Connect(tra)
+
+	msg := []byte("hello world")
+	assert.Nil(t, tra.SendMessage(trb.Addr(), msg))
+
+	rpc := <-trb.Consume()
+	assert.Equal(t, rpc.Payload, msg)
+	assert.Equal(t, rpc.From, tra.Addr())
+}
